2da3a0e9435e255646f3da975d88f8acb7f525ff
Shrawani <141846841+justshraa@users.noreply.github.com>
2024-06-17 18:33:17 +0530

    Update HrList.js

--- src/components/HrList.js
+++ src/components/HrList.js
@@ -5,32 +5,35 @@ import './HrList.css';
 const HrList = ({ hrContacts, deleteHr, setEditHr }) => {
   return (
     <div>
-      <h2>HR List</h2>
-      <ul>
-        {hrContacts.map((hr) => (
-          <li key={hr.id}>
-            <div>
-              <strong>Name:</strong> {hr.name}
-            </div>
-            <div>
-              <strong>Email:</strong> {hr.email}
-            </div>
-            <div>
-              <strong>Contact:</strong> {hr.contact}
-            </div>
-            <div>
-              <strong>Post:</strong> {hr.post}
-            </div>
-            {hr.alternateContact && (
-              <div>
-                <strong>Alternate Contact:</strong> {hr.alternateContact}
-              </div>
-            )}
-            <button onClick={() => setEditHr(hr)}>Edit</button>
-            <button onClick={() => deleteHr(hr.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+        <hr className='border-gray-600 mx-20'/>
+      <p className='text-center text-pretty text-4xl font-bold mt-10 mb-6  tracking-widest'> HR LIST </p>
+      <div className=' rounded-md p'>
+        <ul>
+            {hrContacts.map((hr) => (
+            <li className='bg-white bg-opacity-40 border rounded-xl border-gray-500 mx-32 p-10' key={hr.id}>
+                <div>
+                <strong >Name:</strong> {hr.name}
+                </div>
+                <div>
+                <strong>Email:</strong> {hr.email}
+                </div>
+                <div>
+                <strong>Contact:</strong> {hr.contact}
+                </div>
+                <div>
+                <strong>Post:</strong> {hr.post}
+                </div>
+                {hr.alternateContact && (
+                <div>
+                    <strong>Alternate Contact:</strong> {hr.alternateContact}
+                </div>
+                )}
+                <button className='mx-5 mt-5' onClick={() => setEditHr(hr)}>Edit</button>
+                <button onClick={() => deleteHr(hr.id)}>Delete</button>
+            </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
